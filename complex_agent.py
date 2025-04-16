e6463cb73d74649eb8f0e3a46b6a8c5d358baef1
ziming zeng <ziming.zeng@outlook.com>
2025-04-15 20:46:49 -0400

    bug fix

--- complex_agent.py
+++ complex_agent.py
@@ -71,33 +71,4 @@ async def main():
 
 
 if __name__ == "__main__":
-    # Let's make sure the user has uv installed
-    if not shutil.which("uv"):
-        raise RuntimeError(
-            "uv is not installed. Please install it: https://docs.astral.sh/uv/getting-started/installation/"
-        )
-
-    # We'll run the SSE server in a subprocess. Usually this would be a remote server, but for this
-    # demo, we'll run it locally at http://localhost:8000/sse
-    process: subprocess.Popen[Any] | None = None
-    try:
-        this_dir = os.path.dirname(os.path.abspath(__file__))
-        server_file = os.path.join(this_dir, "server.py")
-
-        print("Starting SSE server at http://localhost:8000/sse ...")
-
-        # Run `uv run server.py` to start the SSE server
-        process = subprocess.Popen(["uv", "run", server_file])
-        # Give it 3 seconds to start
-        time.sleep(3)
-
-        print("SSE server started. Running example...\n\n")
-    except Exception as e:
-        print(f"Error starting SSE server: {e}")
-        exit(1)
-
-    try:
-        asyncio.run(main())
-    finally:
-        if process:
-            process.terminate()
+    asyncio.run(main())
